1df34bcb09729d092ea402bc4da11574e118758d
Galileo Teco <g@80bits.com>
2018-06-16 17:58:30 -0500

    bulk upload file

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -10,6 +10,25 @@
           <br>
           <button type="button" class="btn btn-primary" v-on:click="shortClick">Acortar</button>
           <p></p>
+          <input type="file" name="fileToUpload" @change="processFile($event)">
+          <button type="button" class="btn btn-primary" v-on:click="uploadBulk">Subir</button>
+          <p></p>
+
+          <table class="table">
+            <thead>
+                <tr>
+                    <th>Url Original</th>
+                    <th>Url corta</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="data in listUrlShort">
+                    <td>{{data.url}}</td>
+                    <td><a v-bind:href="data.url_short" class="btn btn-link" target="_blank">{{data.url_short}}</a></td>
+                </tr>
+            </tbody>
+        </table>
+
           <a v-bind:href="url_short" class="btn btn-link" target="_blank">{{url_short}}</a>
           <p></p>
           <router-link to="/all" class="btn btn-link">Ver todas las url</router-link>
@@ -23,7 +42,9 @@ export default {
   data() {
     return {
       txtUrl: "",
-      url_short: ""
+      url_short: "",
+      listUrl: [],
+      listUrlShort: []
     };
   },
   methods: {
@@ -45,6 +66,39 @@ export default {
       });
 
       xhr.open("POST", "http://localhost:3000/api/shortener/url");
+      xhr.setRequestHeader("Content-Type", "application/json");
+
+      xhr.send(data);
+      ///
+    },
+    processFile(event) {
+      var self = this;
+      let file = event.target.files[0];
+      const reader = new FileReader();
+      reader.readAsText(file);
+
+      reader.onload = function(e) {
+        var list = reader.result.split("\n");
+        self.listUrl = list;
+      };
+    },
+    uploadBulk: function() {
+      var self = this;
+      ///
+      var data = JSON.stringify({
+        url_bulk: this.listUrl
+      });
+
+      var xhr = new XMLHttpRequest();
+
+      xhr.addEventListener("readystatechange", function() {
+        if (this.readyState === 4) {
+          let result = JSON.parse(this.responseText);
+          self.listUrlShort = result.content.url_bulk;
+        }
+      });
+
+      xhr.open("POST", "http://localhost:3000/api/shortener/urlbulk");
       xhr.setRequestHeader("Content-Type", "application/json");
 
       xhr.send(data);
